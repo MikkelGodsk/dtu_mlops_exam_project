cbd16adb01f363812adc32d68b771e7e34ca34d2
AnneKDesler <s194333@dtu.dk>
2023-01-12 11:57:52 +0100

    training

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -12,4 +12,6 @@ model = Model(lr)
 wandb.watch(model, log_freq=100)
 model.training_step()
 
+trainer = pl.Trainer(max_epochs=epochs, default_root_dir = 'models', logger=pl.loggers.WandbLogger(project="mnist"), log_every_n_steps = 1)
+trainer.fit(model=model, train_dataloaders=trainloader, val_dataloaders=testloader)
 print("hello world")
