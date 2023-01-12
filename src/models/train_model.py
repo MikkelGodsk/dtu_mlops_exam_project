00d0d47f745967a3ee94c98b350994a30470bc01
AnneKDesler <s194333@dtu.dk>
2023-01-12 11:55:29 +0100

    move to cloud

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,7 +1,7 @@
 import wandb
 from model import Model
 
-wandb.init(project="mlops_exam_project", entity="chrillebon", config="config/dafault_params.yaml")
+wandb.init(project="mlops_exam_project", entity="chrillebon", config="config/default_params.yaml")
 
 lr = wandb.config.lr
 epochs = wandb.config.epochs
