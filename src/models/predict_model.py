74891d4cf836d550fa8ced00fdebf5d0ec240593
AnneKDesler <s194333@dtu.dk>
2023-01-18 13:29:21 +0100

    Model trained on gpu can now be loaded on cpu

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -3,6 +3,7 @@ import torch
 from src.models.model import Model
 from typing import Union, Optional
 from fastapi import FastAPI
+from fastapi import UploadFile, File
 
 app = FastAPI()
 
@@ -10,7 +11,7 @@ app = FastAPI()
 @app.get("/translate/{input}")
 def translate(
     input: str = "Hello world",
-    checkpoint: Optional[str] = "models/epoch=1-step=3750.ckpt" #None,  # "models/epoch=1-step=3750.ckpt"
+    checkpoint: Optional[str] = "models/epoch=0-step=1875-v1.ckpt"
 ):
     DEVICE = "cuda:0" if torch.cuda.is_available() else "cpu"
     strict = True if torch.cuda.is_available() else False
@@ -19,9 +20,8 @@ def translate(
         model = Model()
     else:
         model = Model.load_from_checkpoint(
-            checkpoint_path=checkpoint, map_location=DEVICE, strict=strict,
+            checkpoint_path=checkpoint, map_location=DEVICE,
         )
-        print(dict(model.state_dict()).keys())
 
     return {'en': input, 'de translation': model(input)[0]}
 
@@ -30,8 +30,8 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--input", type=str, help="English string to be translated")
     parser.add_argument(
-        "--checkpoint", default="models/epoch=1-step=3750.ckpt", type=str, help="Path to checkpoint"
-    )  # "models/epoch=1-step=3750.ckpt"
+        "--checkpoint", default="models/epoch=0-step=1875-v1.ckpt", type=str, help="Path to checkpoint"
+    )
     args = parser.parse_args()
     input = args.input
 
