97fc402470062ea34e0b4849a3f0aa69c9bd947f
AnneKDesler <s194333@dtu.dk>
2023-01-18 11:24:03 +0100

    test

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -10,7 +10,7 @@ app = FastAPI()
 @app.get("/translate/{input}")
 def translate(
     input: str = "Hello world",
-    checkpoint: Optional[str] = None,  # "models/epoch=1-step=3750.ckpt"
+    checkpoint: Optional[str] = "models/epoch=1-step=3750.ckpt" #None,  # "models/epoch=1-step=3750.ckpt"
 ):
     DEVICE = "cuda:0" if torch.cuda.is_available() else "cpu"
     strict = True if torch.cuda.is_available() else False
@@ -21,16 +21,17 @@ def translate(
         model = Model.load_from_checkpoint(
             checkpoint_path=checkpoint, map_location=DEVICE, strict=strict,
         )
+        print(dict(model.state_dict()).keys())
 
     return {'en': input, 'de translation': model(input)[0]}
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--checkpoint", default=None, type=Union[str, None], help="Path to checkpoint"
-    )  # "models/epoch=1-step=3750.ckpt"
     parser.add_argument("--input", type=str, help="English string to be translated")
+    parser.add_argument(
+        "--checkpoint", default="models/epoch=1-step=3750.ckpt", type=str, help="Path to checkpoint"
+    )  # "models/epoch=1-step=3750.ckpt"
     args = parser.parse_args()
     input = args.input
 
