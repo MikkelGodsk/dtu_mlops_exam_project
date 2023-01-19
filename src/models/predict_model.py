aa1e00a0a33c396acdb8a41cb6f03b78a558061c
Mikkel <s184399@student.dtu.dk>
2023-01-19 20:45:08 +0100

    fixed flake8 and isort

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -1,10 +1,10 @@
 import argparse
-import os
+# import os
 from typing import Optional
 
-import torch
+# from google.cloud import storage
+# import torch
 from fastapi import FastAPI
-from google.cloud import storage
 
 from src.models.model import Model
 
@@ -13,13 +13,9 @@ app = FastAPI()
 
 def newest_model(bucket):
     blobs = list(bucket.list_blobs())
-    blobs = sorted(
-        blobs,
-        key=lambda blob: str(blob.time_created),
-        reverse=True
-    )
+    blobs = sorted(blobs, key=lambda blob: str(blob.time_created), reverse=True)
     for blob in blobs:
-        if blob.name[-3:] == '.pt':
+        if blob.name[-3:] == ".pt":
             return blob
         return None
 
@@ -32,10 +28,11 @@ def translate(
     """
         If a new model is uploaded to the bucket, the function downloads it when invoked.
     """
+    """
     DEVICE = "cuda:0" if torch.cuda.is_available() else "cpu"
     # strict = True if torch.cuda.is_available() else False
 
-    """client = storage.Client()
+    client = storage.Client()
     bucket = client.get_bucket("model-checkpoints-mlops-exam")
     if checkpoint is None:
         blob = newest_model(bucket)
@@ -47,12 +44,12 @@ def translate(
             os.mkdir('models')
         blob.download_to_filename(filename=filename)
     state_dict = torch.load(filename)  # pickle.loads(blob.download_as_string())
-    model = Model().to(DEVICE) """ # 
-    #Model.load_from_checkpoint(
+    model = Model().to(DEVICE) """  #
+    # Model.load_from_checkpoint(
     # checkpoint_path=checkpoint, map_location=DEVICE,
-    #)  
+    # )
     model = Model()
-    #model.load_state_dict(state_dict)
+    # model.load_state_dict(state_dict)
 
     return {"en": input, "de translation": model(input)[0]}
 
